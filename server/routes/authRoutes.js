0de9082032264e519117bf51595ae793e50dc137
teydd <chemosted@gmail.com>
2025-08-28 12:32:53 +0300

    logout route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express")
-const { signup, verify, signin } = require("../controllers/authControllers")
+const { signup, verify, signin, logout } = require("../controllers/authControllers")
 const route = express.Router()
 
 route.post("/signup",signup)
 route.post("/verify",verify)
 route.post("/signin",signin)
+route.post("/logout",logout)
 module.exports = route
