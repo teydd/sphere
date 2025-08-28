c12f649844f05e9033e0e55fc0ddc038c7ecc37d
teydd <chemosted@gmail.com>
2025-08-28 12:11:23 +0300

    token and cookie generator

---
+++ server/util/generateTokenAndCookie.js
@@ -0,0 +1,12 @@
+const jwt = require ("jsonwebtoken")
+
+const generateTokeAndCookie = async(res, user)=>{
+    const token = jwt.sign({user:user._id, role:user.role},process.env.JWT,{expiresIn:"2d"})
+
+    res.cookie("token",token,{
+        httpOnly:true,
+        samesite:"Strict"
+    })
+}
+
+module.exports={generateTokeAndCookie}
