8a40884fd3434d0f34530802d527861eaa4cb918
teydd <chemosted@gmail.com>
2025-08-30 22:38:45 +0300

    model prettier

--- server/models/authModel.js
+++ server/models/authModel.js
@@ -1,29 +1,31 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: String,
+    verificationTokenExpiresAt: String,
+    resetPasswordToken: String,
+    resetPasswordTokenExpiresAt: String,
+  },
+  { timestamps: true }
+);
 
-const UserSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    name:{
-        type:String,
-        required:true
-    },
-    isVerified:{
-        type:Boolean,
-        default:false
-    },
-    verificationToken:String,
-    verificationTokenExpiresAt:String,
-    resetPasswordToken:String,
-    resetPasswordTokenExpiresAt:String
-}, {timestamps:true})
+const User = mongoose.model("sphere", UserSchema);
 
-const User = mongoose.model("sphere",UserSchema)
-
-module.exports= User
+module.exports = User;
