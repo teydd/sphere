bb3774beeb057ec3e24ead85b4308ba68aa4aa22
teydd <chemosted@gmail.com>
2025-08-30 22:39:40 +0300

    token prettier

--- server/util/generateTokenAndCookie.js
+++ server/util/generateTokenAndCookie.js
@@ -1,12 +1,14 @@
-const jwt = require ("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-const generateTokeAndCookie = async(res, user)=>{
-    const token = jwt.sign({user:user._id, role:user.role},process.env.JWT,{expiresIn:"2d"})
+const generateTokeAndCookie = async (res, user) => {
+  const token = jwt.sign({ user: user._id, role: user.role }, process.env.JWT, {
+    expiresIn: "2d",
+  });
 
-    res.cookie("token",token,{
-        httpOnly:true,
-        samesite:"Strict"
-    })
-}
+  res.cookie("token", token, {
+    httpOnly: true,
+    samesite: "Strict",
+  });
+};
 
-module.exports={generateTokeAndCookie}
+module.exports = { generateTokeAndCookie };
