63328c1c038f500161411cd603e848bd96f03a11
teydd <chemosted@gmail.com>
2025-08-30 22:37:58 +0300

    index prettier

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
-const express = require ("express")
-const mongoose = require ("mongoose")
-const cookieParser = require ("cookie-parser")
-require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
-const auth = require("./routes/authRoutes")
+const auth = require("./routes/authRoutes");
 
-PORT = process.env.PORT || 3000
+PORT = process.env.PORT || 3000;
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/",auth)
+app.use("/", auth);
 
-mongoose.connect(process.env.Sphere).then(()=>{
-    console.log("DB connected suucessfully")
-    app.listen(PORT, ()=>{
-        console.log("Server running in port ",PORT)
-    })
-}).catch(()=>{
-    console.log("DB not connected")
-})
+mongoose
+  .connect(process.env.Sphere)
+  .then(() => {
+    console.log("DB connected suucessfully");
+    app.listen(PORT, () => {
+      console.log("Server running in port ", PORT);
+    });
+  })
+  .catch(() => {
+    console.log("DB not connected");
+  });
