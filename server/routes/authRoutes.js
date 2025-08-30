a83ae35b1795960397d4aa8171d5a47903c912a7
teydd <chemosted@gmail.com>
2025-08-30 22:39:04 +0300

    routes prettier

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,13 +1,21 @@
-const express = require("express")
-const { signup, verify, signin, logout, forgotPassword, resetPassword, checkAuth } = require("../controllers/authControllers")
-const { verifyToken } = require("../middleware/verifyToken")
-const route = express.Router()
+const express = require("express");
+const {
+  signup,
+  verify,
+  signin,
+  logout,
+  forgotPassword,
+  resetPassword,
+  checkAuth,
+} = require("../controllers/authControllers");
+const { verifyToken } = require("../middleware/verifyToken");
+const route = express.Router();
 
-route.post("/signup",signup)
-route.post("/verify",verify)
-route.post("/signin",signin)
-route.post("/logout",logout)
-route.post("/forgot-password",forgotPassword)
-route.post("/reset-password/:token",resetPassword)
-route.get("/auth",verifyToken,checkAuth)
-module.exports = route
+route.post("/signup", signup);
+route.post("/verify", verify);
+route.post("/signin", signin);
+route.post("/logout", logout);
+route.post("/forgot-password", forgotPassword);
+route.post("/reset-password/:token", resetPassword);
+route.get("/auth", verifyToken, checkAuth);
+module.exports = route;
