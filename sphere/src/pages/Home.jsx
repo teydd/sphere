565bf1d28849c9cae3d5345ce1097e1ec8893e8d
teydd <chemosted@gmail.com>
2025-08-31 18:47:51 +0300

    home component

---
+++ sphere/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, error exercitationem vero officiis velit nesciunt tenetur totam expedita consequatur reiciendis. Fugit enim nulla, placeat eum culpa dignissimos rem debitis voluptatem!</p>
+    </>
+  )
+}
