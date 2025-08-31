4992e6629431a6a1bec10395eb6af71e1d5a1be9
teydd <chemosted@gmail.com>
2025-08-31 18:47:17 +0300

    navbar prettier

--- sphere/src/components/Nav.jsx
+++ sphere/src/components/Nav.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function Nav() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Sphere</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">World Map</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Sphere
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  World Map
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
