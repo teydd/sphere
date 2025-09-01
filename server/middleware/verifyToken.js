fb5e4e418f14f28ec84164e0c806f6f26639d0cf
teydd <chemosted@gmail.com>
2025-09-01 23:39:18 +0300

    verify token prettier

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,19 +1,21 @@
-const jwt = require ("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-const verifyToken = async (req,res,next)=>{
-    const token = req.cookies.token
-    if(!token){
-        return res.status(400).json({message:"Unauthorised - no token provided"})
-    }
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res
+      .status(400)
+      .json({ message: "Unauthorised - no token provided" });
+  }
 
-    try {
-        const {userId} = jwt.verify(token,process.env.JWT)
-        req.userId = userId
-        next()
-    } catch (error) {
-        console.log("Error checking auth")
-        res.status(400).json({message : error.message})        
-    }
-}
+  try {
+    const { userId } = jwt.verify(token, process.env.JWT);
+    req.userId = userId;
+    next();
+  } catch (error) {
+    console.log("Error checking auth");
+    res.status(400).json({ message: error.message });
+  }
+};
 
-module.exports = {verifyToken}
+module.exports = { verifyToken };
