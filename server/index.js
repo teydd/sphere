e4500ab28a0da7d1c03c57255bbe6c9065b16bc4
teydd <chemosted@gmail.com>
2025-09-06 23:42:51 +0300

    cors iimplementation

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require ("cors")
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
@@ -10,6 +11,7 @@ PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({origin:"http://localhost:3000"}))
 
 app.use("/", auth);
 
