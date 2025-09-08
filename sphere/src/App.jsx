616866a4bade94c1286d75c415e6bf98f369497d
teydd <chemosted@gmail.com>
2025-09-08 21:22:18 +0300

    app jsx update

--- sphere/src/App.jsx
+++ sphere/src/App.jsx
@@ -1,11 +1,19 @@
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+import Nav from "./components/Nav"
+import Home from './pages/Home';
+import Login from './pages/Login';
 function App() {
   
 
   return (
     <>
-      <p>hfhvb</p>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+    </Router>      
     </>
   )
 }
