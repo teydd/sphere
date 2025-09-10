495bb27630943eb00a6b784e75184d8166a0c376
teydd <chemosted@gmail.com>
2025-09-10 22:33:24 +0300

    login prettier

--- sphere/src/pages/Login.jsx
+++ sphere/src/pages/Login.jsx
@@ -5,7 +5,9 @@ export default function Login() {
     <>
       <div className="container bg-whit mt-5 col-sm-6 col-md-6 col-lg-4 form rounded-5">
         <div className=" text-center">
-          <a className='text-decoration-none text-black lead fw-bold' href="/">Sphere</a>
+          <a className="text-decoration-none text-black lead fw-bold" href="/">
+            Sphere
+          </a>
         </div>
         <p className="text-center pt-3">Sign in to continue</p>
         <hr />
