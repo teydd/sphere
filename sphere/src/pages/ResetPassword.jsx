e6c07ed915e5815440525693996a53dcc0073413
teydd <chemosted@gmail.com>
2025-09-13 22:22:51 +0300

    reset prettier

--- sphere/src/pages/ResetPassword.jsx
+++ sphere/src/pages/ResetPassword.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export default function ResetPassword() {
   return (
     <>
-    <div className="container bg-whit mt-5 col-sm-6 col-md-6 col-lg-4 form rounded-5">
-      <div className=" text-center">
-        <a className="text-decoration-none text-black lead fw-bold" href="/">
-          Sphere
-        </a>
+      <div className="container bg-whit mt-5 col-sm-6 col-md-6 col-lg-4 form rounded-5">
+        <div className=" text-center">
+          <a className="text-decoration-none text-black lead fw-bold" href="/">
+            Sphere
+          </a>
+        </div>
+        <p className="text-center pt-3">Enter new password</p>
+        <hr />
+        <form>
+          <input
+            className="form-control"
+            type="text"
+            name=""
+            id=""
+            placeholder="Email"
+          />{" "}
+          <hr />
+          <button className="btn btn-outline-dark w-100">Submit</button>
+          <hr />
+        </form>
+        <br />
       </div>
-      <p className="text-center pt-3">Enter new password</p>
-      <hr />
-      <form>
-        <input
-          className="form-control"
-          type="text"
-          name=""
-          id=""
-          placeholder="Email"
-        />{" "}
-        <hr />
-        <button className="btn btn-outline-dark w-100">Submit</button>
-        <hr />
-      </form>
-      <br />
-    </div>
     </>
-  )
+  );
 }
