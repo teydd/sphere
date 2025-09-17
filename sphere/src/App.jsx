be4551c7f6e5374c4f0c788f08cb7115f4f8ac96
teydd <chemosted@gmail.com>
2025-09-17 22:57:39 +0300

    prettier

--- sphere/src/App.jsx
+++ sphere/src/App.jsx
@@ -1,28 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
-import Nav from "./components/Nav"
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import ForgotPassword from './pages/ForgotPassword';
-import ResetPassword from './pages/ResetPassword';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 function App() {
-  
-
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/signin' element={<Login></Login>}></Route>
-        <Route path='/signup' element={<Signup></Signup>}></Route>
-        <Route path='/forgot-password' element={<ForgotPassword></ForgotPassword>}></Route>
-        <Route path='/reset-password' element={<ResetPassword></ResetPassword>}></Route>
-      </Routes>
-    </Router>      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/signin" element={<Login></Login>}></Route>
+          <Route path="/signup" element={<Signup></Signup>}></Route>
+          <Route
+            path="/forgot-password"
+            element={<ForgotPassword></ForgotPassword>}
+          ></Route>
+          <Route
+            path="/reset-password"
+            element={<ResetPassword></ResetPassword>}
+          ></Route>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
