c057341a10ea226cc79021eb4c1ad72cf01b2a74
teydd <chemosted@gmail.com>
2025-09-17 22:54:10 +0300

    login link

--- sphere/src/components/Nav.jsx
+++ sphere/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Nav() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg">
+      <nav className="navbar navbar-expand-sm">
         <div className="container-fluid">
           <a className="navbar-brand" href="/world"></a>
           <button
@@ -29,29 +29,7 @@ export default function Nav() {
                 </a>
               </li>
             </ul>
-
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-
-
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+            <a className="btn bg-white border border-white  text-black float-end" href="/signin">Signin</a>
           </div>
         </div>
       </nav>
