4b5e62a3204421c9f0340dff92714f354d0f1128
teydd <chemosted@gmail.com>
2025-09-18 23:46:04 +0300

    origin edit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:"http://localhost:3000"}))
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
 
 app.use("/", auth);
 
