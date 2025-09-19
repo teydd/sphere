be3de176d4bb8eecb6dc7299daef7c64fb346269
teydd <chemosted@gmail.com>
2025-09-19 20:19:32 +0300

    prettier

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require ("cors")
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
@@ -11,7 +11,7 @@ PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/", auth);
 
