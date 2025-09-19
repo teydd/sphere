4dca059ee82183d4639d7d9d99e0ff222a0b0c6c
teydd <chemosted@gmail.com>
2025-09-19 20:20:04 +0300

    prettier

--- sphere/src/pages/Login.jsx
+++ sphere/src/pages/Login.jsx
@@ -23,7 +23,12 @@ export default function Login() {
     <>
       <div className="container bg-whit mt-5 col-sm-6 col-md-6 col-lg-4 form rounded-5">
         <div className=" text-center">
-          <Link className="text-decoration-none text-black lead fw-bold" to={"/"}>Sphere</Link>
+          <Link
+            className="text-decoration-none text-black lead fw-bold"
+            to={"/"}
+          >
+            Sphere
+          </Link>
         </div>
         <p className="text-center pt-3">Sign in to continue</p>
         <hr />
@@ -46,13 +51,17 @@ export default function Login() {
             placeholder="Password"
           />
           <br />
-          <Link className="nav-link text-center" to={"/forgot-password"}>Forgot Password?</Link>
+          <Link className="nav-link text-center" to={"/forgot-password"}>
+            Forgot Password?
+          </Link>
           <hr />
           <button className="btn btn-outline-dark w-100">Submit</button>
           <hr />
           <p className="text-center">
             Don't have an account?{" "}
-            <Link className="text-black text-decoration-none" to={"/signup"}>Signup</Link>
+            <Link className="text-black text-decoration-none" to={"/signup"}>
+              Signup
+            </Link>
           </p>
         </form>
         <br />
