f16d92ecbd3e4fe4e95a729e73e4550bd31c35aa
teydd <chemosted@gmail.com>
2025-09-19 20:19:49 +0300

    prettier

--- sphere/src/components/Nav.jsx
+++ sphere/src/components/Nav.jsx
@@ -20,10 +20,20 @@ export default function Nav() {
           <div className="collapse navbar-collapse " id="navbarNav">
             <ul className="navbar-nav align-items-center justify-content-center w-100">
               <li className="nav-item ">
-                <Link className="fs-1 text-white title text-decoration-none" to={"/"}>Sphere</Link>
+                <Link
+                  className="fs-1 text-white title text-decoration-none"
+                  to={"/"}
+                >
+                  Sphere
+                </Link>
               </li>
             </ul>
-            <Link className="btn bg-white border border-white  text-black float-end" to={"/signin"}>Signin</Link>
+            <Link
+              className="btn bg-white border border-white  text-black float-end"
+              to={"/signin"}
+            >
+              Signin
+            </Link>
           </div>
         </div>
       </nav>
