a372735d410c41bd3936cf4e0d755fa1c5991674
teydd <chemosted@gmail.com>
2025-09-19 20:20:12 +0300

    prettier

--- sphere/src/store/authStore.js
+++ sphere/src/store/authStore.js
@@ -1,25 +1,36 @@
-import { create } from "zustand"
-import axios from "axios"
+import { create } from "zustand";
+import axios from "axios";
 
-const URL = "http://localhost:3000"
+const URL = "http://localhost:3000";
 
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true;
 
-export const useAuthStore = create((set)=>({
-    user:null,
-    isAuthenticated:false,
-    error:null,
-    isLoading:false,
-    isCheckingAuth:true,
+export const useAuthStore = create((set) => ({
+  user: null,
+  isAuthenticated: false,
+  error: null,
+  isLoading: false,
+  isCheckingAuth: true,
 
-    signup: async(email,password,name)=>{
-        set({isLoading:true,error:null})
-        try {
-            const response = await axios.post(`${URL}/signup`,{email,password,name})
-            set({user:response.data.user,isAuthenticated:true,isLoading:false})
-        } catch (error) {
-            set({error:error.response.data.message || "Error signing up",isLoading:false})
-            throw error
-        }
+  signup: async (email, password, name) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${URL}/signup`, {
+        email,
+        password,
+        name,
+      });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({
+        error: error.response.data.message || "Error signing up",
+        isLoading: false,
+      });
+      throw error;
     }
-}))
+  },
+}));
