6ef3410bce4efd13280f8088b82e0e6b0e1750cc
teydd <chemosted@gmail.com>
2025-10-16 23:17:02 +0300

    prettier

--- server/Mail/resendMail.js
+++ server/Mail/resendMail.js
@@ -1,15 +1,14 @@
-const { Resend } = require('resend');
-require("dotenv").config()
-
+const { Resend } = require("resend");
+require("dotenv").config();
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendVerificationEmail = async (email, name, token) => {
   try {
     await resend.emails.send({
-      from: 'Sphere@resend.dev',
+      from: "Sphere@resend.dev",
       to: email,
-      subject: 'Verify Your Email',
+      subject: "Verify Your Email",
       html: `
         <p>Hi ${name},</p>
         <p>Use the code below to verify your email:</p>
@@ -17,25 +16,25 @@ const sendVerificationEmail = async (email, name, token) => {
       `,
     });
   } catch (err) {
-    console.error('Failed to send verification email:', err);
+    console.error("Failed to send verification email:", err);
   }
 };
 
-const welcomeEmail = async(name,email)=>{
+const welcomeEmail = async (name, email) => {
   try {
     await resend.emails.send({
-     from: 'Sphere@resend.dev',
+      from: "Sphere@resend.dev",
       to: email,
-      subject: 'Welcome to Sphere',
+      subject: "Welcome to Sphere",
       html: `
         <p>Hi ${name},</p>
         <p>Welcome t0 Sphere, a weather app that's one of a kind </p>
         <p>Follow the link to see wants in store for you</p>
         <a>Explore the Application</a>
       `,
-    })
+    });
   } catch (error) {
-    console.error("Error sending welcome email")    
+    console.error("Error sending welcome email");
   }
-}
-module.exports = {sendVerificationEmail,welcomeEmail};
+};
+module.exports = { sendVerificationEmail, welcomeEmail };
