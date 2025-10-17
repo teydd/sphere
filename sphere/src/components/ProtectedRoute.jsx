824ff8659c1d6af721ca612e93f85c5830161508
teydd <chemosted@gmail.com>
2025-10-17 08:22:36 +0300

    update

--- sphere/src/components/ProtectedRoute.jsx
+++ sphere/src/components/ProtectedRoute.jsx
@@ -5,7 +5,13 @@ import { Navigate } from 'react-router-dom'
 export const ProtectedRoute = ({children}) => {
     const {isAuthenticated, isCheckingAuth} = useAuthStore()
     if(isCheckingAuth){
-        return
+        return(
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="spinner-border text-primary" role="status">
+          <span className="visually-hidden">Checking authentication...</span>
+        </div>
+      </div>
+    );
     }
   return isAuthenticated ? children :<Navigate to={"/signin"}></Navigate>
 }
