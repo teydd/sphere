9264f5077d62caa99d642c8acd79bb963db0e059
teydd <chemosted@gmail.com>
2025-10-17 12:47:29 +0300

    google auth implementation

--- sphere/src/main.jsx
+++ sphere/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {GoogleOAuthProvider} from "@react-oauth/google"
 
+
+const CLIENT_ID = import.meta.env.VITE_REACT_APP_ID
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
