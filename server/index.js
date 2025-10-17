3cc04761e609a22821487104a9647c05a32b2266
teydd <chemosted@gmail.com>
2025-10-17 08:16:41 +0300

    helmet and express rate limit installation and origin changed

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const helmet = require('helmet');
+const rateLimit = require ("express-rate-limit")
 require("dotenv").config();
 
 const auth = require("./routes/authRoutes");
@@ -11,7 +13,12 @@ PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(helmet())
+app.use(rateLimit({
+  max:50,
+  windowMS: 10*60*1000
+}))
 
 app.use("/", auth);
 
