f31fe985980ea1135300d48c0d3d1a15302df847
teydd <chemosted@gmail.com>
2025-10-17 08:19:26 +0300

    tempalte usage

--- server/Mail/resendMail.js
+++ server/Mail/resendMail.js
@@ -1,19 +1,16 @@
 const { Resend } = require("resend");
+const { PASSWORD_RESET_REQUEST_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } = require("./templates");
 require("dotenv").config();
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendVerificationEmail = async (email, name, token) => {
+const sendVerificationEmail = async (email, token) => {
   try {
     await resend.emails.send({
       from: "Sphere@resend.dev",
       to: email,
       subject: "Verify Your Email",
-      html: `
-        <p>Hi ${name},</p>
-        <p>Use the code below to verify your email:</p>
-        <h2>${token}</h2>
-      `,
+      html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}",token),
     });
   } catch (err) {
     console.error("Failed to send verification email:", err);
@@ -28,7 +25,7 @@ const welcomeEmail = async (name, email) => {
       subject: "Welcome to Sphere",
       html: `
         <p>Hi ${name},</p>
-        <p>Welcome t0 Sphere, a weather app that's one of a kind </p>
+        <p>Welcome to Sphere, a weather app that's one of a kind </p>
         <p>Follow the link to see wants in store for you</p>
         <a>Explore the Application</a>
       `,
@@ -37,4 +34,33 @@ const welcomeEmail = async (name, email) => {
     console.error("Error sending welcome email");
   }
 };
-module.exports = { sendVerificationEmail, welcomeEmail };
+
+const forgotPass = async(email, resetToken)=>{
+  try {
+    await resend.emails.send({
+      from: "Sphere@resend.dev",
+      to: email,
+      subject: "Forgot Password Link",
+      html:PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}",resetToken)
+    });
+  } catch (error) {
+    console.error("Error sending forgot password link");
+    console.log(error)
+  }
+
+}
+
+const resetPass = async(email)=>{
+   try {
+    await resend.emails.send({
+      from: "Sphere@resend.dev",
+      to: email,
+      subject: "Reset Password Successful",
+      html:PASSWORD_RESET_SUCCESS_TEMPLATE
+    });
+  } catch (error) {
+    console.error("Error sending forgot password link");
+    console.log(error)
+  }
+}
+module.exports = { sendVerificationEmail, welcomeEmail,forgotPass, resetPass };
